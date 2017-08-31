076c0ff142b9ed229febc43c3f2fc03da8944bc1
David Richardson <davidr@ebi.ac.uk>
2017-08-31 09:35:58 +0100

    migrated submission envelope from subs-repository

---
+++ src/main/java/uk/ac/ebi/subs/dispatcher/SubmissionEnvelopeService.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.subs.dispatcher;
+
+import uk.ac.ebi.subs.processing.SubmissionEnvelope;
+import uk.ac.ebi.subs.repository.model.StoredSubmittable;
+
+import java.util.stream.Stream;
+
+public interface SubmissionEnvelopeService {
+
+    SubmissionEnvelope fetchOne(String submissionId);
+    Stream<? extends StoredSubmittable> submissionContents(String submissionId);
+}
